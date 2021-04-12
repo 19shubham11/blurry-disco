fb7ec749152fd2feeede0581f0ba3db256c6f1f9
Shubham Chaturvedi <19shubham11@gmail.com>
2021-04-12 23:20:59 +0200

    cleanup

--- pkg/redis/redisSetup.go
+++ pkg/redis/redisSetup.go
@@ -2,14 +2,11 @@ package redis
 
 import (
 	"19shubham11/url-shortener/cmd/conf"
-	"fmt"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func SetupRedis(config conf.RedisConf) *redis.Client {
-	// redisUrl := fmt.Sprintf("redis://%s:%s@%s:%d/%d", config.Username, config.Password, config.Host, config.Port, config.DB)
-	_ = fmt.Sprintf("%s:%d", config.Host, config.Port)
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Username: config.Username,
